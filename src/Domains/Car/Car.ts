ec21dfecbe0850fe2bd7f8f548b8ec1a93c49822
Philip Lages <psilvaelages@gmail.com>
2023-02-13 17:15:25 -0300

    feat: creates car service

--- src/Domains/Car/Car.ts
+++ src/Domains/Car/Car.ts
@@ -1,77 +1,29 @@
 import ICar from '../../Interfaces/ICar';
 
 export default class Car {
-  private _model: string;
-  private _year: number;
-  private _color: string;
-  private _status?: boolean;
-  private _buyValue: number;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
   private _doorsQty: number;
   private _seatsQty: number;
 
   constructor(car: ICar) {
-    this._model = car.model;
-    this._year = car.year;
-    this._color = car.color;
-    this._status = car.status || false;
-    this._buyValue = car.buyValue;
+    this.model = car.model;
+    this.year = car.year;
+    this.color = car.color;
+    this.status = car.status || false;
+    this.buyValue = car.buyValue;
     this._doorsQty = car.doorsQty;
     this._seatsQty = car.seatsQty;
   }
 
-  public set model(model: string) {
-    this._model = model;
-  }
-
-  public get model() {
-    return this._model;
-  }
-
-  public set year(year: number) {
-    this._year = year;
-  }
-
-  public get year() {
-    return this._year;
-  }
-
-  public set color(color: string) {
-    this._color = color;
-  }
-
-  public get color() {
-    return this._color;
-  }
-
-  public set status(status: boolean | undefined) {
-    this._status = status;
-  }
-
-  public get status() {
-    return this._status;
-  }
-
-  public set buyValue(buyValue: number) {
-    this._buyValue = buyValue;
-  }
-
-  public get buyValue() {
-    return this._buyValue;
-  }
-
-  public set doorsQty(doorsQty: number) {
-    this._doorsQty = doorsQty;
-  }
-
-  public get doorsQty() {
+  protected get doorsQty() {
     return this._doorsQty;
   }
 
-  public set seatsQty(seatsQty: number) {
-    this._seatsQty = seatsQty;
-  }
-
-  public get seatsQty() {
+  protected get seatsQty() {
     return this._seatsQty;
   }
 }
