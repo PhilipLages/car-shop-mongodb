508d2cad5dfde2a3076529481ca5181b67cf5c3e
Philip Lages <psilvaelages@gmail.com>
2023-02-13 17:30:26 -0300

    feat: creates error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/Errorhandler';
 import carRoutes from './Routes/Car.routes';
 
 const app = express();
 
 app.use('/cars', carRoutes);
+app.use(ErrorHandler.handle);
 
 export default app;
