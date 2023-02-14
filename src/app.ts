30dd9bc16e1d8d959d47a4b2bfd8424cecfc609c
Philip Lages <psilvaelages@gmail.com>
2023-02-13 21:49:29 -0300

    feat: adds requirement 5

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/Errorhandler';
 import carRoutes from './Routes/Car.routes';
+import motorcycleRoutes from './Routes/Motorcycle.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', carRoutes);
+app.use('/motorcycles', motorcycleRoutes);
 app.use(ErrorHandler.handle);
 
 export default app;
